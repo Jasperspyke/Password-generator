9bfc82d65c99510ed60bc3499cf15b37b36500db
Jasperspyke <104863800+Jasperspyke@users.noreply.github.com>
2022-08-07 11:14:21 -0400

    Update PSW generator.py

--- PSW generator.py
+++ PSW generator.py
@@ -1,53 +1,66 @@
-import random
 import sys
-dict={}
-wordchoices='cool,nondescript,shoe,victorious,uptight,guiltless,relate,peaceful,soggy,nifty,birds,roll,drink,accurate,rend,course,throat,nurse,rind,productive,horrible,shiny,tame,typical,fluffy'
-words=wordchoices.split(',')
+import random
 
-characters='!@#$%^&*()'
-splitchar=[char for char in characters]
-numbers='1234567890'
-digits=[num for num in numbers]
+try:
+    old_dict = eval(str(open('PSW.txt', 'r').read()))
+except SyntaxError:
+    old_dict = {'Foo': 'Bar'}
+wordchoices = 'cool,nondescript,shoe,gangster,victorious,uptight,guiltless,relate,peaceful,soggy,nifty,birds,roll,drink,accurate,rend,course,throat,nurse,rind,productive,horrible,shiny,tame,typical,fluffy'
+words = wordchoices.split(',')
 
-class password:
-    def __init__(self,string1=None,string2=None,numbers=None, characters=None):
-        self.string1=string1
-        self.string2=string2
-        self.numbers=numbers
-        self.characters=characters
+characters = '!@#$%^&*()'
+splitchar = [char for char in characters]
+numbers = '1234567890'
+digits = [num for num in numbers]
 
-    def createpw(self,service):
-        string1=random.choice(words)
-        string2=random.choice(words)
-        numbers=[random.choice(digits) for i in range(1,5)]
-        numbers=''.join(numbers)
-        characters=[random.choice(splitchar) for i in range (1,5)]
-        characters=''.join(characters)
 
-        pw=string1+string2+numbers+characters
-        if service not in dict:
-            dict[servicestring]=pw
-            print(f'Your password for {servicestring} is {pw}')
-        else:
-            print('Password already created for this service!')
-            sys.exit()
-print('Type exit to exit without saving and save to exit and save dictionary of passwords.')
-while True:
-    service=input('What service would you like to create a password for:'  )
-    servicestring=str(service)
-    if servicestring.lower()=='exit':
+class Password:
+    def __init__(self, string1=None, string2=None, numbers=None, characters=None):
+        self.string1 = string1
+        self.string2 = string2
+        self.numbers = numbers
+        self.characters = characters
+
+    def createpw(self):
+        string1 = random.choice(words)
+        string2 = random.choice(words)
+        numbers = [random.choice(digits) for i in range(1, 5)]
+        numbers = ''.join(numbers)
+        characters = [random.choice(splitchar) for i in range(1, 5)]
+        characters = ''.join(characters)
+
+        pw = string1.upper() + string2 + numbers + characters
+
+        return pw
+
+
+print('Type exit to exit without saving. Otherwise, type the name of the service to create password for')
+
+
+def main():
+    global old_dict
+    service = input('What service would you like to create a password for:')
+    servicestring = str(service)
+    if servicestring.lower() == 'exit':
         print('cancelling program')
         sys.exit()
 
-    elif servicestring.lower()=='save':
+    elif servicestring in old_dict.keys():
+        service = Password(service)
+        old_dict[servicestring] = service.createpw()
+        print('dictionary rewritten with new value')
 
-        with open('PSW.txt','a') as file:
-            file.write(str(dict))
-            print('Password dictionary successfully saved')
-            print(dict)
-            sys.exit()
     else:
-        service=password(service)
-        service.createpw(service)
+        service = Password(service)
+        old_dict[servicestring] = service.createpw()
+        print('new password created')
+    if 'Foo' in old_dict.keys():
+        del old_dict['Foo']
+
+    with open('PSW.txt', 'w') as f:
+
+        f = f.write(str(old_dict))
+        print('done')
 
 
+main()
